3ee70addbfea440ba968a3c23869797c2e825496
Muhammed <muhammed@sendme.ng>
2022-06-16 12:08:03 +0100

    Initial fulfillment for Guide Bot

--- Electricity-Bot/index.js
+++ Electricity-Bot/index.js
@@ -5,7 +5,7 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 const {Card, Suggestion } = require('dialogflow-fulfillment');
 const axios = require('axios');
 
-process.env.DEBUG = 'dialogflow:debig';
+process.env.DEBUG = 'dialogflow:debug';
  exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({ request, response });
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
