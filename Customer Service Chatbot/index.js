5ada36dc58830cf7387c51c1d8c6939ffe7254d9
Muhammed <muhammed@sendme.ng>
2022-06-16 12:30:26 +0100

    Push to staging

--- Customer Service Chatbot/index.js
+++ Customer Service Chatbot/index.js
@@ -1,1 +1,16 @@
+'use strict';
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const axios = require('axios');
+const { AgentsClient } = require('dialogflow/src/v2');
 
+process.env.DEBUG = "dialogflow: debug";
+    exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+        console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+        console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+
+    let intentMap = new Map();
+    agent.handleRequest(intentMap);
+    })
