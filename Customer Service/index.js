5e68b9d8ad4f21ecc7c29656f1bb3c5d2f3da31c
Muhammed <muhammed@sendme.ng>
2022-06-16 12:44:35 +0100

    Initial fulfillment for Service Bot

--- Customer Service/index.js
+++ Customer Service/index.js
@@ -1,1 +1,14 @@
+'use strict';
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const axios = require('axios');
 
+process.env.DEBUG = "dialogflow: debug";
+    exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+        console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+        console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    let intentMap = new Map();
+    agent.handleRequest(intentMap);
+    })
